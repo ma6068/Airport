3a840999c586163fa3794953610c57fa1eddd23a
ma6068 <ma6068@student.uni-lj.si>
2021-09-17 14:39:32 +0200

    tests

--- venv/Include/main.py
+++ venv/Include/main.py
@@ -10,35 +10,41 @@ import json
 
 
 def getCityAndTime(url):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get(url)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
-    html_code = soup.prettify()
-    # get the times
-    times = re.findall("<div class=\"detail-table__cell text-center fdabf-td1\">([^<]*)</div>", html_code)
-    for i in range(len(times)):
-        times[i] = times[i].strip()   # delete empty spaces in string
-    times.pop()  # last element is not a time => delete it
-    # get the cities
-    cities = re.findall("<span class=\"visible-xs\">([^<]*)</span>", html_code)
-    for i in range(len(cities)):
-        cities[i] = cities[i].strip()  # delete empty spaces in string
-    cities = cities[: len(cities) - 2] # last two elements are not a city => delete it
-    return times, cities
+    try:
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        driver.quit()
+        html_code = soup.prettify()
+        # get the times
+        times = re.findall("<div class=\"detail-table__cell text-center fdabf-td1\">([^<]*)</div>", html_code)
+        for i in range(len(times)):
+            times[i] = times[i].strip()   # delete empty spaces in string
+        times.pop()  # last element is not a time => delete it
+        # get the cities
+        cities = re.findall("<span class=\"visible-xs\">([^<]*)</span>", html_code)
+        for i in range(len(cities)):
+            cities[i] = cities[i].strip()  # delete empty spaces in string
+        cities = cities[: len(cities) - 2] # last two elements are not a city => delete it
+        return times, cities
+    except:
+        print('Something went wrong')
 
 def getTemperature(cities):
-    api_key = 'b8dffc5cce80c763531d05464a0f37e0'
-    temperatures = []
-    for city in cities:
-        apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + api_key
-        try:
-            json_data = requests.get(apiUrl).json()
-            temperature = (int)(json_data['main']['temp'] - 273.15)  # from kelvin to celsius
-        except:
-            temperature = 'No info'  # no temperature info for that city
-        temperatures.append(temperature)
-    return temperatures
+    try:
+        api_key = 'b8dffc5cce80c763531d05464a0f37e0'
+        temperatures = []
+        for city in cities:
+            apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + api_key
+            try:
+                json_data = requests.get(apiUrl).json()
+                temperature = (int)(json_data['main']['temp'] - 273.15)  # from kelvin to celsius
+            except:
+                temperature = 'No info'  # no temperature info for that city
+            temperatures.append(temperature)
+        return temperatures
+    except:
+        print('Something went wrong')
 
 def createNotes(temperatures, cities):
     notes = []
@@ -89,6 +95,12 @@ def createNotesFromJson(json_data):
     return createNotes(temperatures, cities)
 
 
+def test3(times, cities):
+    for i in range(len(times)):
+        if not times[i] or not cities[i]:
+            return False
+    return True
+
 if __name__ == "__main__":
     url = 'https://www.viennaairport.com/passagiere/ankunft__abflug/abfluege'
     times, cities = getCityAndTime(url)
@@ -104,10 +116,14 @@ if __name__ == "__main__":
     db.createTable()
     for i in range(len(times)):
         db.insert_airport(times[i], cities[i], temperatures[i], notes[i])
-    # print(db.get_all_airports())
+    print(db.get_all_airports())
 
     #################################### Caching ####################################
-    createCache(times, cities, temperatures)
-    json_data = readCache()
-    notes = createNotesFromJson(json_data)
+    # createCache(times, cities, temperatures)
+    # json_data = readCache()
+    # notes = createNotesFromJson(json_data)
     # print(notes)
+
+    #################################### Test ####################################
+    # check if we get the time and cities (they are not null)
+    # if test3(times, cities): print('OK')
--- venv/Include/test.py
+++ venv/Include/test.py
@@ -1,4 +1,5 @@
 import db
+import json
 
 try:
     os.delete('airport.db')
@@ -7,7 +8,60 @@ except:
 
 db = db.DB()
 db.createTable()
-db.insert_airport('12:30', 'Mardir', '23', 'So cool!!!')
-result = db.get_all_airports()
-if ('12:30', 'Mardir', 23, 'So cool!!!') in result:
-    print('Ok')
+
+def readCache():
+    with open('cache_data.txt') as json_file:
+        data_set = json.load(json_file)
+        return data_set
+
+def createNote(city, temperature):
+    note = ''
+    # if there is no info for temperature
+    if temperature == 'No info':
+        note = 'No temperature information'
+    # Spain
+    elif (city == 'Barcelona' or city == 'Madrid') and temperature >= 22:
+        note = 'Hace mucho calor.'
+    elif (city == 'Barcelona' or city == 'Madrid') and temperature < 22:
+        note = 'No hace mucho calor.'
+    # Germany
+    elif (city == 'Frankfurt' or city == 'Stuttgart' or city == 'Leipzig') and temperature >= 22:
+        note = 'Es ist sehr heiß.'
+    elif (city == 'Frankfurt' or city == 'Stuttgart' or city == 'Leipzig') and temperature < 22:
+        note = 'Es ist nicht sehr heiß.'
+    # Other
+    elif temperature >= 22:
+        note = 'It\'s very hot.'
+    else:
+        note = 'It is not very hot.'
+    return note
+
+# check if inserting in database is ok
+def test1():
+    db.insert_airport('12:30', 'Mardir', '23', 'So cool!!!')
+    result = db.get_all_airports()
+    if ('12:30', 'Mardir', 23, 'So cool!!!') in result:
+        return True
+    return False
+
+# check if note in database and cached note are the same
+def test2():
+    result = db.get_all_airports()
+    ok_list = []
+    for i in range(len(result)):
+        time, city, temperature, note1 = result[0]
+        json_data = readCache()
+        for key in json_data:
+            if json_data[key][1] == city and json_data[key][2] == temperature:
+                note2 = createNote(city, temperature)
+                if note1 == note2:
+                    ok_list.append(True)
+                else:
+                    ok_list.append(False)
+    if False in ok_list:
+        return False
+    return True
+
+
+if (test1()): print('OK')
+if (test2()): print('OK')  # for this test we need files db.py and cache_data.txt
